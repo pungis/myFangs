8d0d4e57da9b85826758e58357c5cda4caa4a365
Filip Brzezicki <filipbrzezicki@macbook-air-filip.home>
2021-10-31 09:13:19 +0100

    pre commit

--- my_fangs/src/view/WelcomeScreen/WelcomeScreen.tsx
+++ my_fangs/src/view/WelcomeScreen/WelcomeScreen.tsx
@@ -1,23 +1,26 @@
-import React from "react";
-import logo from "../../assets/icons/myFangsIcon.svg";
-import { StyledWelcomeScreen, StyledWelcomeScreenHeader, StyledWelcomeScreenLogo, StyledLink  } from "./WelcomeScreenStyled"
+import React from 'react';
+import logo from '../../assets/icons/myFangsIcon.svg';
+import {
+  StyledWelcomeScreen,
+  StyledWelcomeScreenHeader,
+  StyledWelcomeScreenLogo,
+  StyledLink,
+} from './WelcomeScreenStyled';
 
 function handleDetection(event: any) {
   console.log('change');
   console.log(event.target);
-
 }
 
 const WelcomeScreen = (props: any) => {
   return (
     <StyledWelcomeScreen>
       <StyledWelcomeScreenHeader>
-        <StyledWelcomeScreenLogo src={logo} alt="logo" />
+        <StyledWelcomeScreenLogo src={logo} alt='logo' />
         <h1>My Fangs</h1>
+
         <p>tarantulas dairy</p>
-        <StyledLink to="/dashboard">
-          Dashboard
-        </StyledLink>
+        <StyledLink to='/dashboard'>Dashboard</StyledLink>
       </StyledWelcomeScreenHeader>
     </StyledWelcomeScreen>
   );
